1756f3382e01504d4e20a0050261c3bf18721910
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2022-01-02 05:28:30 +0700

    add home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { Welcome, TanamPohon, Leaderboard } from "./homes";
+
 export default function Home() {
   return (
     <>
-      <div className="container pt-5">
-        <h1 className="mt-4">Home page</h1>
-      </div>
+      <Welcome />
+      <TanamPohon />
+      <Leaderboard />
     </>
   );
 }
---
+++ src/pages/homes/index.js
@@ -0,0 +1,3 @@
+export { default as Welcome } from "./Welcome";
+export { default as TanamPohon } from "./TanamPohon";
+export { default as Leaderboard } from "./Leaderboard";
