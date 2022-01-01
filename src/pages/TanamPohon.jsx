b27858441aa8e83f2c80094cd599e6518b3e9b7d
deviayulestari <devialestari04@gmail.com>
2022-01-01 22:02:47 +0700

    add tanam pohon

--- src/pages/TanamPohon.jsx
+++ src/pages/TanamPohon.jsx
@@ -1,10 +1,97 @@
 import React from "react";
+import "../assets/styles/TanamPohon.css"
 
 export default function TanamPohon() {
   return (
     <>
-      <div className="container pt-5">
-        <h1 className="mt-4">Tanam Pohon page</h1>
+      <div className="tanam-pohon container-fluid p-0">
+        <section className="header-tp">
+          <div className="content-header-tp">
+            <h1 className="title-tp">Tanam Pohon</h1>
+            <h3 className="subtitle-tp">Hijaukan bumimu! Bagaimana Caranya?</h3>
+            <p className="text-header-tp">Greenpeace Indonesia menyediakan ruang bagi kamu yang ingin kembali menguhijaukan bumi kita dengan cara menanam pohon. Ayo berkontribusi untuk membuat bumi menjadi lebih baik!</p>
+            <button className="btn-tanam-pohon">Mulai Menanam Pohon</button>
+          </div>
+        </section>
+        <section className="content-tp-2">
+          <div className="row row-tp-2">
+            <div className="col-lg-8">
+              <h1>Saya mau menanam pohon, apa yang harus saya persiapkan?</h1>
+              <ul>
+                <li>Mendaftar dan mengisi data diri.</li>
+                <li>Datang ke tempat Tanam Pohon yang kamu pilih.</li>
+                <li>Dokumentasikan kegiatan untuk bukti mengikuti kegiatan kemudian unggah ke formulir yang di sediakan.</li>
+                <li>Informasi tambahan:
+                    Tanam Pohon ini merupakan program berkala yang bertujuan untuk menahan laju suhu iklim global.</li>
+              </ul>
+            </div>
+            <div className="col-lg-4">
+              <img className=" img-tp" src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"></img>
+            </div>
+          </div>
+        </section>
+        <section className="content-tp-3">
+          <div className="row text-section-3">
+            <div className="col-lg-9 text-header-tp-3">
+              <h1>Mari mulai Tanam Pohon!</h1>
+            </div>
+            <div className="col-lg-3">
+              <button className="btn-tp-3">Lebih Banyak</button>
+            </div>
+          </div>
+          <div className="cards-tp">
+            <div className="card-tp">
+              <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+              <div className="circle-share-tp">
+                <i class="fas fa-share-alt"></i>
+              </div>
+              <p><i class="fas fa-map-marked-alt"></i> Papua Barat</p>
+              <p><i class="fas fa-calendar-alt"></i> 20 April 2022</p>
+              <h1>Tanam pohon untuk papua yang lebih hijau</h1>
+              <button className="btn-join-tp">Bergabung</button>
+            </div>
+            <div className="card-tp">
+              <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+              <div className="circle-share-tp">
+                <i class="fas fa-share-alt"></i>
+              </div>
+              <p><i class="fas fa-map-marked-alt"></i> Papua Barat</p>
+              <p><i class="fas fa-calendar-alt"></i> 20 April 2022</p>
+              <h1>Tanam pohon untuk papua yang lebih hijau</h1>
+              <button className="btn-join-tp">Bergabung</button>
+            </div>
+            <div className="card-tp">
+              <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
+              <div className="circle-share-tp">
+                <i class="fas fa-share-alt"></i>
+              </div>
+              <p><i class="fas fa-map-marked-alt"></i> Papua Barat</p>
+              <p><i class="fas fa-calendar-alt"></i> 20 April 2022</p>
+              <h1>Tanam pohon untuk papua yang lebih hijau</h1>
+              <button className="btn-join-tp">Bergabung</button>
+            </div>
+          </div>
+        </section>
+        <section className="content-tp-4">
+          <div className="row text-section-4">
+            <div className="col-lg-9 text-header-tp-4">
+              <h1>Dokumentasi Tanam Pohon</h1>
+            </div>
+            <div className="col-lg-3">
+              <button className="btn-tp-4">Lebih Banyak</button>
+            </div>
+          </div>
+            <div className="row dokumetasi-content">
+              <div className="col-lg-6 doc-column-1">
+                <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"></img>
+              </div>
+              <div className="col-lg-6 doc-column-2">
+              <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"></img>
+              <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"></img>
+              <img src="https://images.unsplash.com/photo-1598335624134-5bceb5de202d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"></img>
+              </div>
+            </div>
+        </section>
       </div>
     </>
   );
