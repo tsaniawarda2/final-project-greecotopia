408043d894b91f4ea24ba6411c5f404384ea9f2b
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2022-01-01 21:59:59 +0700

    updare proile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
 import Logo from "../assets/image/logo.png";
+import Profile from "../assets/image/profile.png";
+import Point from "../assets/image/point.png";
 import "../assets/styles/navbar.css";
 
 export default function Navbar() {
@@ -8,6 +11,7 @@ export default function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div className="container">
+          {/* LEFT */}
           <NavLink className="navbar-brand" to="/">
             <img className="logo-nav" src={Logo} alt="brand" /> Greecotopia
           </NavLink>
@@ -23,6 +27,7 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {/* CENTER */}
             <ul className="navbar-nav mx-auto mb-md-2 mb-lg-0 navCenter">
               <li className="nav-item me-md-3">
                 <NavLink exact className="nav-link" aria-current="page" to="/">
@@ -45,7 +50,9 @@ export default function Navbar() {
                 </NavLink>
               </li>
             </ul>
+            {/* RIGHT */}
             <ul className="navbar-nav mb-md-5 mb-lg-0 navRight">
+              {/* Form */}
               <li className="nav-item me-md-4">
                 <NavLink
                   className="nav-link text-uppercase signup"
@@ -59,6 +66,22 @@ export default function Navbar() {
                   Masuk
                 </NavLink>
               </li>
+              {/* Account */}
+              {/* <li className="nav-item">
+                <NavLink
+                  className="nav-link text-uppercase account"
+                  to="/account"
+                >
+                  <div className="profile1">
+                    <img id="avatar" src={Profile} alt="profile" />
+                  </div>
+                  <div className="profile2">
+                    <p className="username-user">Siti Mae</p>
+                    <img id="point" src={Point} alt="point" />
+                    1000 poin
+                  </div>
+                </NavLink>
+              </li> */}
             </ul>
           </div>
         </div>
