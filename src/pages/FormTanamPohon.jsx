f30a5841cf951dd2fbb1e1bbff367fa074e716d9
deviayulestari <devialestari04@gmail.com>
2022-01-03 15:23:44 +0700

    add style in form tanam pohon

--- src/pages/FormTanamPohon.jsx
+++ src/pages/FormTanamPohon.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "../assets/styles/FormTanamPohon.css"
+import "../assets/styles/formTanamPohon.css"
 
 export default function FormTanamPohon() {
   return (
@@ -16,39 +16,50 @@ export default function FormTanamPohon() {
             </thead>
             <tbody>
               <tr>
-                <td>Date </td>
-                <td> : </td>
+                <td>Tanggal </td>
+                <td className="titik-dua-tp"> : </td>
                 <td>20 April 2022</td>
               </tr>
               <tr>
-                <td>Time</td>
-                <td> : </td>
+                <td>Pukul</td>
+                <td className="titik-dua-tp"> : </td>
                 <td> 08.00 WIT</td>
               </tr>
               <tr>
-                <td>Location </td>
-                <td> : </td>
+                <td>Lokasi </td>
+                <td className="titik-dua-tp"> : </td>
                 <td>Taman wisata alam Sorong, kota Sorong, Papua Barat </td>
               </tr>
               <tr>
                 <td>Hadiah Poin</td>
-                <td> : </td>
-                <td>150 poin/pohon</td>
+                <td className="titik-dua-tp"> : </td>
+                <td>150 Poin/Pohon</td>
               </tr>
-            </tbody>
-          </table>
-          <p>Jangan lupa untuk mengambil gambar saat kegiatan, kemudian unggah foto tersebut di bagian unggah dokumentasi.</p>
-          <table>
-            <tbody>
               <tr>
-                <td>Periode tanggal unggah foto</td>
-                <td> : </td>
+                <td>Periode unggah foto</td>
+                <td className="titik-dua-tp"> : </td>
                 <td>20 April 2022 -23 April 2022</td>
               </tr>
             </tbody>
           </table>
+          <br></br>
+          <p>Jangan lupa untuk mengambil gambar saat kegiatan, kemudian unggah foto tersebut di bagian unggah dokumentasi pada periode yang ditentukan.</p>
           <div className="syarat-ketentuan-tp">
             <p> <i class="fas fa-info-circle"></i> Syarat & Ketentuan</p>
+          </div>
+          <div className="tp-info row">
+            <div className="col-4">
+              <p>Trees Planted</p>
+              <h6 className="tp-info-1">1.000</h6>
+            </div>
+            <div className="col-4">
+              <p>Hectares Restored</p>
+              <h6 className="tp-info-2">1.5</h6>
+            </div>
+            <div className="col-4">
+              <p>Since</p>
+              <h6 className="tp-info-3">2021</h6>
+            </div>
           </div>
         </div>
         <div className="form-tp col-lg-6">
@@ -57,21 +68,15 @@ export default function FormTanamPohon() {
             <form action="">
               <div className="form-tp-nama">
               <label for="nama">Nama</label>
-                <input className="form-control" type="text" placeholder="Nama Kamu"/>
+                <input className="form-control" type="text" placeholder=""/>
               </div>
               <div className="form-tp-no-hp">
                 <label for="noHp">No. Handphone</label>
-                <input className="form-control" type="text" placeholder="Nomor Handphone Kamu"/>
+                <input className="form-control" type="text" placeholder=""/>
               </div>
               <div class="form-tp-jumlah-pohon">
                 <label for="jumlahPohon">Jumlah Pohon</label>
-                <select class="form-control" id="jumlahPohon">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
+                <input class="form-control" type="number" min="1" max="100"></input>
               </div>
               <div className="form-tp-lokasi">
                 <label for="lokasi">Lokasi</label>
@@ -79,9 +84,9 @@ export default function FormTanamPohon() {
               </div>
               <div className="form-check">
                 <input type="checkbox" className="form-check-input" id="siap-tp"/>
-                <label className="form-check-label" for="siap-tp">Saya siap menanam pohon</label>
+                <p className="form-check-label" for="siap-tp">Saya siap menanam pohon</p>
               </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
+              <button type="submit" className="btn-daftar">DAFTAR</button>
             </form>
           </div>
         </div>
