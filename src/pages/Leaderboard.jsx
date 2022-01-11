a5d38a19162e9016f8bae5cfcc80ff3d6fc0272a
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2022-01-11 17:27:46 +0700

    styling

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import Avatar from "react-avatar";
+
 import "../assets/styles/leaderboard.css";
+
+const dataUser = [
+  {
+    message: "Success Get Data Top 10",
+    users: [
+      {
+        user_id: 3,
+        username: "kasdf",
+        image_url: "",
+        points: 900,
+      },
+      {
+        user_id: 2,
+        username: "jojO4567",
+        image_url:
+          "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJveXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60",
+        points: 6400,
+      },
+      {
+        user_id: 1,
+        username: "budiMan",
+        image_url: "",
+        points: 4000,
+      },
+    ],
+  },
+];
+const dataTopTen = dataUser[0].users;
+console.log(dataTopTen, "----");
 export default function Leaderboard() {
   return (
     <>
@@ -19,15 +50,39 @@ export default function Leaderboard() {
               buruan kumpulkan poinnya!
             </p>
             {/* Button */}
-            <NavLink
-              to="/formReward"
-              className="btn btn-light text-center"
-              id="btn-home"
-            >
-              Top 3? Klaim Hadiahmu!
-            </NavLink>
 
-            <div className="board"></div>
+            <div className="board">
+              {/* Main User */}
+
+              {/* Button Claim */}
+              <NavLink
+                to="/formReward"
+                className="btn btn-danger text-center"
+                id="btnClaimB"
+              >
+                Top 3? <br /> Klaim Hadiahmu!
+              </NavLink>
+
+              {/* Child User */}
+              {dataTopTen?.map((user) => (
+                <>
+                  <div className="colomnU">
+                    <div className="textUser">
+                      <p id="rankUser">4</p>
+                      <div id="imgUser">
+                        {user?.image_url ? (
+                          <Avatar src={user?.image_url} id="avaUser" />
+                        ) : (
+                          <Avatar name={user?.username} id="avaUser" />
+                        )}
+                      </div>
+                      <p id="userName">{user?.username}</p>
+                    </div>
+                    <p id="poinUser">{user?.points} Poin</p>
+                  </div>
+                </>
+              ))}
+            </div>
           </div>
         </div>
       </section>
